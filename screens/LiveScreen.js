a4f07b9c8d4adb8c5e07b9649593963d50066484
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2019-07-01 10:05:37 -0400

    working and displaying

--- screens/LiveScreen.js
+++ screens/LiveScreen.js
@@ -2,32 +2,39 @@ import React,{Component} from 'react';
 import {View,Text} from 'react-native';
 
 export default class LiveScreen extends Component{
-  componentWillMount(){
+  constructor(props){
+    super(props);
+    this.state = {
+      days:'',
+      hours:'',
+      minutes:'',
+      seconds:'',
+      something:'Hi'
+    }
+  }
+  componentDidMount(){
     this.countDownClock();
   }
-  countDownClock(){
-    // get todays date and time
-    // get the difference
-    var hours = new Date().getHours();
-    var minutes = new Date().getMinutes();
-    var seconds = new Date().getSeconds();
-    var day = new Date().getDate();
-    var month = new Date().getMonth()+1;
-    var year = new Date().getFullYear();
-    var nextEvent = new Date('2019/07/05');
-    var dayz = new Date(`${year}/${month}/${day}`);
-    var diff = Math.abs(nextEvent - dayz);
-    var secondsTillEvent = diff / 1000;
-    var minutesTillEvent = secondsTillEvent/60;
-    var hoursTillEvent = minutesTillEvent/60;
-    var daysTillEvent = hoursTillEvent / 24;
-    var something = `${daysTillEvent}:${hoursTillEvent /24 }:${minutesTillEvent/60}:${secondsTillEvent/60}`;
-    console.log(minutesTillEvent/60/24)
+  countDownClock=()=>{
+    var countDownDate = new Date('July 5, 2019 19:30:00').getTime();
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance %(1000 * 60 * 60 *24))/(1000 * 60 * 60));
+    var minutes = Math.floor((distance %(1000 * 60 * 60)) /(1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60))/1000);
+    var clock = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    this.setState({
+      days:days,
+      hours:hours,
+      minutes:minutes,
+      seconds:seconds,
+    });
   }
   render(){
     return(
       <View>
-        <Text>Live Screen</Text>
+        
       </View>
     )
   }
